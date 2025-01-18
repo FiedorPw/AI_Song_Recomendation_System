64844f81cf3cff7ab59fd614e49eff29de852e75
olarynk <01168953@pw.edu.pl>
2025-01-18 19:24:43 +0100

    zmiana testów AB

--- testA,B/AB_test.py
+++ testA,B/AB_test.py
@@ -1,20 +1,18 @@
 import pandas as pd
 
 def precision_recall(tab):
-    relevant_recommendations = tab["reakcja"].apply(lambda x: 1 if x == "like" else 0)
-    recommendations = tab["reakcja"].apply(lambda x: 1 if x else 0)
+    relevant_recommendations = tab["reaction"].apply(lambda x: 1 if x == "like" else 0)
+    recommendations = tab["reaction"].apply(lambda x: 1 if x else 0)
     accuracy = relevant_recommendations.sum() / recommendations.sum() if not recommendations.empty else 0
     return accuracy
 
 if __name__=="__main__":
     tab = pd.read_csv("tab_AB.csv")
     tab_knn = tab[tab["model"] == "knn"]
-    tab_neuMF = tab[tab["model"] == "NeuMF"]
-    tab_brak = tab[tab["model"] == "brak"]
+    tab_neuMF = tab[tab["model"] == "ncf"]
     knn_accuracy = precision_recall(tab_knn)
     print("Dokładność modelu knn: ", knn_accuracy)
     neuMF_accuracy = precision_recall(tab_neuMF)
-    print("Dokładność modelu neuMF: ", neuMF_accuracy)
-    brak_accuracy = precision_recall(tab_brak)
-    print("Dokładność losowego modelu: ", brak_accuracy)
+    print("Dokładność modelu ncf: ", neuMF_accuracy)
+
 
